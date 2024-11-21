1497b1aef239ad4a833601bc432303e37fcd98f1
laxmi kant joshi <lkjoshi.tech@gmail.com>
2024-11-21 00:57:41 +0000

    some changes

--- EmployeeSpringBoot/src/main/java/com/EmployeeSpringBoot/employee/repository/IEmployeeDetails.java
+++
@@ -1,8 +0,0 @@
-package com.EmployeeSpringBoot.employee.repository;
-
-import com.EmployeeSpringBoot.employee.entity.EmployeeDetails;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface IEmployeeDetails extends MongoRepository<EmployeeDetails, ObjectId> {
-}
